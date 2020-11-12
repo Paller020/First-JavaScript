eb1b75aad5682ea12d1856b890ed71bb5829b1f3
Paller020 <paller21016@columbushs.comgit config --global user.name Your>
2020-11-12 12:12:50 -0500

    114 finished

--- JavaScript.js
+++ JavaScript.js
@@ -1,4 +1,13 @@
-function obama(){
-
-    d
+var name = "Susy";
+if (name === "Billy"|| name === "Susy"){
+    alert("hi Billy!");
 }
+
+else if (name === "Susy"){
+    alert("Hi Susy!")
+}
+
+else{
+    alert("Who are you!");
+}
+
