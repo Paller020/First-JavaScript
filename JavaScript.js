ecf6e943579baf1bc84cee953fa204a0be4538d3
Paller020 <paller21016@columbushs.comgit config --global user.name Your>
2020-11-12 12:28:16 -0500

    115 exercise done

---
+++ JavaScript.js
@@ -0,0 +1,15 @@
+var age = prompt("How old are you?");
+
+
+if(Number(age) < 18){
+    alert("Sorry, you are too young to drive this car. Powering off");
+}
+else if(Number(age) > 18){
+    alert("Powering On. Enjoy the ride!");
+} 
+else if(Number(age) === 18){
+    alert("Congratulations on your first year of driving. Enjoy the ride!");
+}
+else{
+    alert("Pleas put a correct value!")
+}
