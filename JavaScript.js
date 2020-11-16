5d7ae941338072842b54eaadd210a1509e7f7bb0
Paller020 <paller21016@columbushs.comgit config --global user.name Your>
2020-11-16 12:08:24 -0500

    119 finished

--- JavaScript.js
+++ JavaScript.js
@@ -1,14 +1,35 @@
-function multiply(a, b){
 
-if(a > 10 || b> 10 ){
-    return "that's too hard";
-} 
-else{
-    return a*b;
+function checkDriverAge(age) {
+	var age = prompt("What is your age?");
+	if (Number(age) < 18) {
+		alert("Sorry, you are too yound to drive this car. Powering off");
+	} else if (Number(age) > 18) {
+		alert("Powering On. Enjoy the ride!");
+	} else if (Number(age) === 18) {
+		alert("Congratulations on your first year of driving. Enjoy the ride!");
+	}
 }
-    return a*b;
+checkDriverAge();
 
+var checkDriverAge2 = function() {
+	var age = prompt("What is your age?");
+	if (Number(age) < 18) {
+		alert("Sorry, you are too yound to drive this car. Powering off");
+	} else if (Number(age) > 18) {
+		alert("Powering On. Enjoy the ride!");
+	} else if (Number(age) === 18) {
+		alert("Congratulations on your first year of driving. Enjoy the ride!");
+	}
 }
 
-var total = multiply(5,10);
-alert(total);
+checkDriverAge2();
+
+function checkDriverAge3(age) {
+    if (Number(age) < 18) {
+       return "Sorry, you are too yound to drive this car. Powering off";
+   } else if (Number(age) > 18) {
+       return "Powering On. Enjoy the ride!";
+    } else if (Number(age) === 18) {
+       return "Congratulations on your first year of driving. Enjoy the ride!";
+   }
+   }
