fe4e253857c6bda17efbd3fe8357e7602a1d1e84
Paller020 <paller21016@columbushs.comgit config --global user.name Your>
2020-11-16 11:26:59 -0500

    117 done

--- JavaScript.js
+++ JavaScript.js
@@ -1,16 +1,12 @@
-var age = prompt("How old are you?");
+4+3;
 
+if(4+3 === 7){
+    console.log("Hellooooooo");
+}
 
-if(Number(age) < 18){
-    alert("Sorry, you are too young to drive this car. Powering off");
-}
-else if(Number(age) > 18){
-    alert("Powering On. Enjoy the ride!");
-} 
-else if(Number(age) === 18){
-    alert("Congratulations on your first year of driving. Enjoy the ride!");
-}
-else{
-    alert("Pleas put a correct value!")
-}
-s
+console.log("Hellooooooo");
+console.log("Hellooooooo");
+console.log("Hellooooooo");
+console.log("Hellooooooo");
+console.log("Hellooooooo");
+console.log("Hellooooooo");
